c4fb00341cab3d0be0a2617afe93607187a3b87c
matcha <meen.sankaew@gmail.com>
2021-01-03 17:53:54 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,39 +2,46 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const jsdom = require("jsdom");
-axios
-  .post("https://freemovies.centos.vercel.app/api/load_index", { page: 1 })
-  .then(({ data }) => {
-    fs.writeFileSync(
-      path.join(__dirname, "..", "public", "indexCached.json"),
-      JSON.stringify(data)
-    );
-  });
+// axios
+//   .post("https://freemovies.centos.vercel.app/api/load_index", { page: 1 })
+//   .then(({ data }) => {
+//     fs.writeFileSync(
+//       path.join(__dirname, "..", "public", "indexCached.json"),
+//       JSON.stringify(data)
+//     );
+//   });
 
 axios
   .post("https://freemovies.centos.vercel.app/api/load_index", { page: 1 })
   .then(({ data }) => {
-    let i;
-    let out = [];
-    for (i = 0; i < data.length; i++) {
-      (async () => {
-        const _res = await axios.get("https://movie285.com/" + data[i].href);
-        const dom = new jsdom.JSDOM(_res.data);
-        out.push({
-          src: dom.window.document
-            .getElementsByClassName("video-box")[0]
-            .querySelector("iframe").src,
-          title: dom.window.document
-            .getElementsByClassName("titlex")[0]
-            .querySelector("h1").innerHTML,
-          synopsis: dom.window.document
-            .querySelector(".x-content")
-            .innerHTML.replace(/(<([^>]+)>)/gi, ""),
-        });
-      })();
-    }
-    fs.writeFileSync(
-      path.join(__dirname, "..", "public", "movie.json"),
-      JSON.stringify(out)
-    );
+    Promise.all(
+      data.map((movie) => {
+        return axios.get("https://movie285.com/" + movie.href);
+      })
+    ).then((dataList) => {
+      fs.writeFileSync(
+        path.join(__dirname, "..", "public", "movie.json"),
+        JSON.stringify(
+          dataList.map((_data) => {
+            const dom = new jsdom.JSDOM(_data.data);
+            console.log(
+              dom.window.document
+                .getElementsByClassName("titlex")[0]
+                .querySelector("h1").innerHTML
+            );
+            return {
+              src: dom.window.document
+                .getElementsByClassName("video-box")[0]
+                .querySelector("iframe").src,
+              title: dom.window.document
+                .getElementsByClassName("titlex")[0]
+                .querySelector("h1").innerHTML,
+              synopsis: dom.window.document
+                .querySelector(".x-content")
+                .innerHTML.replace(/(<([^>]+)>)/gi, ""),
+            };
+          })
+        )
+      );
+    });
   });
