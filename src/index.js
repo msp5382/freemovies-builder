ce4051668d3c9f769c2410d120e9a264e97e9316
matcha <meen.sankaew@gmail.com>
2021-01-03 17:57:43 +0700

    change sources

--- src/index.js
+++ src/index.js
@@ -10,38 +10,36 @@ const jsdom = require("jsdom");
 //       JSON.stringify(data)
 //     );
 //   });
-
-axios
-  .post("https://freemovies.centos.vercel.app/api/load_index", { page: 1 })
-  .then(({ data }) => {
-    Promise.all(
-      data.map((movie) => {
-        return axios.get("https://movie285.com/" + movie.href);
+const movies = require("./movie.json");
+console.log(movies["movie"].length);
+Promise.all(
+  movies["movie"].map((movie) => {
+    return axios.get("https://movie285.com/" + movie.href);
+  })
+).then((dataList) => {
+  console.log(dataList.length);
+  fs.writeFileSync(
+    path.join(__dirname, "..", "public", "movie.json"),
+    JSON.stringify(
+      dataList.map((_data) => {
+        const dom = new jsdom.JSDOM(_data.data);
+        console.log(
+          dom.window.document
+            .getElementsByClassName("titlex")[0]
+            .querySelector("h1").innerHTML
+        );
+        return {
+          src: dom.window.document
+            .getElementsByClassName("video-box")[0]
+            .querySelector("iframe").src,
+          title: dom.window.document
+            .getElementsByClassName("titlex")[0]
+            .querySelector("h1").innerHTML,
+          synopsis: dom.window.document
+            .querySelector(".x-content")
+            .innerHTML.replace(/(<([^>]+)>)/gi, ""),
+        };
       })
-    ).then((dataList) => {
-      fs.writeFileSync(
-        path.join(__dirname, "..", "public", "movie.json"),
-        JSON.stringify(
-          dataList.map((_data) => {
-            const dom = new jsdom.JSDOM(_data.data);
-            console.log(
-              dom.window.document
-                .getElementsByClassName("titlex")[0]
-                .querySelector("h1").innerHTML
-            );
-            return {
-              src: dom.window.document
-                .getElementsByClassName("video-box")[0]
-                .querySelector("iframe").src,
-              title: dom.window.document
-                .getElementsByClassName("titlex")[0]
-                .querySelector("h1").innerHTML,
-              synopsis: dom.window.document
-                .querySelector(".x-content")
-                .innerHTML.replace(/(<([^>]+)>)/gi, ""),
-            };
-          })
-        )
-      );
-    });
-  });
+    )
+  );
+});
